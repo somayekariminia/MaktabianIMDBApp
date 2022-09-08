55750c3485d6ebe9b31459a6acb8532e038857ca
somaye6868 <somayekariminia6868@gmail.com>
2022-09-08 18:47:04 +0430

    update createCommentTable method

--- src/repository/TableCreatorRepository.java
+++ src/repository/TableCreatorRepository.java
@@ -40,7 +40,7 @@ public class TableCreatorRepository {
                 " FOREIGN KEY(user_id) REFERENCES user_table(id)," +
                 " FOREIGN KEY(film_id) REFERENCES film_table(id))");
     }
-    public  void watchedFilm() throws SQLException {
+    public  void watchedFilmTable() throws SQLException {
         Connection connection = ConnectionGate.getConnection();
         PreparedStatement PreparedStatement=connection.prepareStatement("create table watched_table(id int primary key," +
                 "user_id int," +
--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -1,27 +1,58 @@
 package repository;
 
+import model.Film;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserRepository {
 
-    public boolean login(String userName,String password) throws SQLException {
+    public boolean login(String userName, String password) throws SQLException {
         Connection connection = ConnectionGate.getConnection();
-        PreparedStatement preparedStatement=connection.prepareStatement("select * from user_table" +
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from user_table" +
                 " where userName=? and password=?");
-        preparedStatement.setString(1,userName);
-        preparedStatement.setString(2,password);
-        ResultSet resultSet= preparedStatement.executeQuery();
-        boolean flage=false;
-        while (resultSet.next()){
-            if(!resultSet.getString("userName").equals(null) &&
+        preparedStatement.setString(1, userName);
+        preparedStatement.setString(2, password);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        boolean flage = false;
+        while (resultSet.next()) {
+            if (!resultSet.getString("userName").equals(null) &&
                     !resultSet.getString("password").equals(null))
-                flage=true;
+                flage = true;
         }
         return flage;
     }
 
+    public List<Film> myFavoriteFilm() throws SQLException {
+        Connection connection = ConnectionGate.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from user_table INNER JOIN favorite_Film " +
+                " on user_table.id=favorite_Film.user_id  inner join film_table on film_table.id=favorit_Film.film_id");
+           List<Film> filmList=new ArrayList<>();
+        ResultSet resultSet=preparedStatement.executeQuery();
+        while (resultSet.next()){
+            Film film=new Film();
+            filmList.add(film);
+        }
+        return  filmList;
+    }
+    public List<Film> myWatchedFilm() throws SQLException {
+        Connection connection = ConnectionGate.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("(select * from user_table INNER JOIN favorite_Film " +
+                " on user_table.id=favorite_Film.user_id  inner join film_table " +
+                "on film_table.id=favorit_Film.film_id)");
+        List<Film> filmList=new ArrayList<>();
+        ResultSet resultSet=preparedStatement.executeQuery();
+        while (resultSet.next()){
+            Film film=new Film();
+            filmList.add(film);
+        }
+        return  filmList;
+    }
+public List<Film> myGenreFavorite(){
 
+}
 }
