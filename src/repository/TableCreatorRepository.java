3d5654a74491f06f39f0cfe0f7bdb8f2b6ae3a80
somaye6868 <somayekariminia6868@gmail.com>
2022-09-08 16:58:12 +0430

    add watchFilmTable

--- src/repository/TableCreatorRepository.java
+++ src/repository/TableCreatorRepository.java
@@ -40,5 +40,14 @@ public class TableCreatorRepository {
                 " FOREIGN KEY(user_id) REFERENCES user_table(id)," +
                 " FOREIGN KEY(film_id) REFERENCES film_table(id))");
     }
+    public  void watchedFilm() throws SQLException {
+        Connection connection = ConnectionGate.getConnection();
+        PreparedStatement PreparedStatement=connection.prepareStatement("create table watched_table(id int primary key," +
+                "user_id int," +
+                "film_id int," +
+                " FOREIGN KEY(user_id) REFERENCES user_table(id)," +
+                " FOREIGN KEY(film_id) REFERENCES film_table(id))");
+    }
+
 }
 
