2ad4b59b7bea480832281da90c3fcd30e27b2e29
somaye6868 <somayekariminia6868@gmail.com>
2022-09-08 16:25:05 +0430

    add User

---
+++ src/model/User.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String userName;
+    private int age;
+    private  String password;
+    private String mobileNumber;
+    private String email;
+    private List<Film> favoriteFilms=new ArrayList<>();
+    private List<Film> watchFilm=new ArrayList<>();
+    private Genre favoriteGenre;
+}
