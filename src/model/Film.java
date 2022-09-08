ab256ecb9440c589e18255a297802e30225c7405
somaye6868 <somayekariminia6868@gmail.com>
2022-09-08 16:28:33 +0430

    add Film calss

---
+++ src/model/Film.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.Date;
+
+public class Film {
+    private int id;
+    private String name;
+    private Genre genre;
+    private String directorName;
+   private Date createdYear;
+   private String country;
+   private  Comment comment;
+   private double rate;
+   private double duration;
+   private AgeCategory ageCategory;
+}
