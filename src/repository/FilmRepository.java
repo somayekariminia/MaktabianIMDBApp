ff41d12a5b9ce609186d4fca238d7f843d7d1cb1
somaye6868 <somayekariminia6868@gmail.com>
2022-09-15 08:32:21 +0430

    edit id film_table autoincreament

--- src/repository/FilmRepository.java
+++ src/repository/FilmRepository.java
@@ -1,6 +1,7 @@
 package repository;
 
 import model.Film;
+import model.Genre;
 import model.User;
 
 import java.sql.Connection;
@@ -13,30 +14,27 @@ import java.util.List;
 public class FilmRepository {
     public void addFilmToFavoriteList(int id, User user, Film film) throws SQLException {
         Connection connection = ConnectionGate.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("insert into favorite_table values(?,?,?)");
-        preparedStatement.setInt(1, id);
-        preparedStatement.setInt(2, user.getId());
-        preparedStatement.setInt(3, film.getId());
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into favorite_table(user_id,film_id) values(?,?)");
+        preparedStatement.setInt(1, user.getId());
+        preparedStatement.setInt(2, film.getId());
         preparedStatement.executeQuery();
     }
 
-    public void addFilmToWatchedList(int id,User user,Film film) throws SQLException {
+    public void addFilmToWatchedList(int id, User user, Film film) throws SQLException {
         Connection connection = ConnectionGate.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("insert into watched_table values(?,?,?)");
-        preparedStatement.setInt(1, id);
-        preparedStatement.setInt(2, user.getId());
-        preparedStatement.setInt(3, film.getId());
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into watched_table(user_id,film_id) values(?,?)");
+        preparedStatement.setInt(1, user.getId());
+        preparedStatement.setInt(2, film.getId());
         preparedStatement.executeQuery();
     }
 
-    public void addCommentToFilm(String comment, int rate,User user,Film film,int id) throws SQLException {
+    public void addCommentToFilm(String comment, int rate, User user, Film film, int id) throws SQLException {
         Connection connection = ConnectionGate.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("insert into comment_table values(?,?,?,?,?)");
-        preparedStatement.setInt(1, id);
-        preparedStatement.setString(2, comment);
-        preparedStatement.setInt(3, rate);
-        preparedStatement.setInt(4, user.getId());
-        preparedStatement.setInt(5, film.getId());
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into comment_table (comment,rate,user_id,film_id) values(?,?,?,?)");
+        preparedStatement.setString(1, comment);
+        preparedStatement.setInt(2, rate);
+        preparedStatement.setInt(3, user.getId());
+        preparedStatement.setInt(4, film.getId());
         preparedStatement.executeQuery();
     }
 
@@ -44,25 +42,27 @@ public class FilmRepository {
         Connection connection = ConnectionGate.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("select rate from commant_table where film_id=?");
         preparedStatement.setInt(1, id);
-        ResultSet resultSet =preparedStatement.executeQuery();
-        List<Integer> list=new ArrayList<>();
-        while (resultSet.next()){
-            int rate=resultSet.getInt("rate");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        List<Integer> list = new ArrayList<>();
+        while (resultSet.next()) {
+            int rate = resultSet.getInt("rate");
             list.add(rate);
         }
-    return list;
+        return list;
     }
-    public  boolean isWatched(String name) throws SQLException {
+
+    public boolean isWatched(String name) throws SQLException {
         Connection connection = ConnectionGate.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("select * from film_table inner join " +
                 " watch_table on whatch_table.film_id=film_table.id where name=? ");
-        preparedStatement.setString(1,name);
-        ResultSet resultSet=preparedStatement.executeQuery();
-        if(resultSet.equals(null))
-          return false;
+        preparedStatement.setString(1, name);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.equals(null))
+            return false;
         else
             return true;
     }
+
     public List<Film> getWatchedFilm() throws SQLException {
         Connection connection = ConnectionGate.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("(select * from user_table INNER JOIN favorite_Film " +
@@ -79,6 +79,7 @@ public class FilmRepository {
         }
         return filmList;
     }
+
     public List<Film> getFavoriteGenresFilms() throws SQLException {
         Connection connection = ConnectionGate.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("select * from user_table INNER JOIN genre_table" +
@@ -95,6 +96,7 @@ public class FilmRepository {
         }
         return list;
     }
+
     public List<Film> getFavoriteFilm() throws SQLException {
         Connection connection = ConnectionGate.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("select * from user_table INNER JOIN favorite_Film " +
@@ -109,6 +111,20 @@ public class FilmRepository {
             filmList.add(film);
         }
         return filmList;
+    }
+
+    public List<Genre> getFavoriteGenres() throws SQLException {
+        Connection connection = ConnectionGate.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("select genre from user_table INNER JOIN genre_table" +
+                " on user_table.id=genre_table.user_id  inner join film_table " +
+                "on film_table.id=gener.film_id  group by genre");
+        List<Genre> list = new ArrayList<>();
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            Genre genre = Genre.valueOf(resultSet.getString("genre"));
+            list.add(genre);
+        }
+        return list;
     }
 }
 
